ac1611e55ad8c4dec5bf047c694f23f4e1db5a4a
Peter J Han <peterjhan88@gmail.com>
2020-02-03 15:07:49 -0800

    Update myRegression.py

--- myRegression.py
+++ myRegression.py
@@ -1,9 +1,9 @@
-# 데이터처리를 위한 패키지
+# packages for processing data
 import pandas as pd
 import numpy as np
 import math
 
-# VIF계산을 위한 패키지
+# packages for VIF calculations
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 from sklearn.model_selection import train_test_split
@@ -14,12 +14,12 @@ from scipy import stats
 
 
 
-# MAD 기반 이상치(outlier)제거
+# MAD based outlier elimination
 def mad_based_outlier(points, thresh=3.5):
     '''
-    thresh : 정규분포표로 했을 때, 상/하위 몇 퍼센트를 쳐낼지 결정(기본 : 3.5프로)
+    thresh : using standard deviation, this value dictates how much pecentage will be eliminated(default: 3.5%)
     points : np.series
-    반환 : 각 행의 포인트가 이상치인지 여부를 True/Fale로 구성된 series반환
+    return : returns if the value of each row is an outlier in True/False in series.
     '''
     if len(points.shape) == 1:
         points = points[:,None]
@@ -32,16 +32,16 @@ def mad_based_outlier(points, thresh=3.5):
     # https://medium.com/james-blogs/outliers-make-us-go-mad-univariate-outlier-detection-b3a72f1ea8c7
     modified_z_score = 0.6745 * diff / med_abs_deviation
     return modified_z_score > thresh 
-# 출처: https://pythonanalysis.tistory.com/7 [Python 데이터 분석]
+# reference: https://pythonanalysis.tistory.com/7 [Python Data Analysis]
 
 
 
-# 다중공선성(Multicollinearity)의 최소화를 위한 분산팽창인자(Variance Inflation Factor, VIF) 확인 함수
+# Functionto check Variance Inflation Factor(VIF) to minimize Multicollinearity
 def lowVIF(df, n=7, cols =['temp', 'cloud', 'wind','humid', 'hpa', 'sun_time', 'lgt_time', 
        'SO2', 'CO', 'O3', 'NO2', 'PM10', 'PM25'] ):
     '''
-    cols_using : df의 컬럼에서 VIF를 계산하고 싶은 columns을 넣으면 된다.
-    기본은 우리팀에서 쓰는 날씨 변수들이 들어가 있다.
+    cols_using : put names of column, in array, which you want to caculate VIF
+                by default, column names that relates to the project is set
     '''
     col_to_use = cols
     vif = pd.DataFrame()
@@ -51,11 +51,11 @@ def lowVIF(df, n=7, cols =['temp', 'cloud', 'wind','humid', 'hpa', 'sun_time', '
     vif.sort_values("VIF_Factor")
     lowest_vif = vif.sort_values("VIF_Factor")[:n].reset_index()
     lowest_vif.drop(columns='index', inplace=True)
-    # df로 반환해준다. 0번 column에 변수명, 1번 column에 VIF값
+    # returns in df. 0: column name, 1: value of VIF
     return lowest_vif
 
 #########################################################################
-# modeling 함수로 만들어 처리하기
+# function to process modeling 
 def linReg(df, item, cols):
     cols_using = cols
     X = df.loc[df['category']==item, cols_using]
@@ -65,9 +65,9 @@ def linReg(df, item, cols):
 
     model = LinearRegression().fit(X_train, y_train)
   
-    print('LinearRegression을 이용한 %s의 회귀분석 결과 :'%item)
-    print('훈련세트점수 : {:.2f}'.format(model.score(X_train, y_train)))
-    print('검증세트점수 : {:.2f}'.format(model.score(X_test, y_test)))
+    print('Analysis result of %s using LinearRegression :'%item)
+    print('Training Set Score : {:.2f}'.format(model.score(X_train, y_train)))
+    print('Verificaiton Set Score : {:.2f}'.format(model.score(X_test, y_test)))
 
     
 def ridgeReg(df, item, cols_using):
@@ -79,9 +79,9 @@ def ridgeReg(df, item, cols_using):
 
     ridge = Ridge(alpha=0.1, normalize=True, random_state=0, tol=0.001).fit(X_train, y_train)
     
-    print('RidgeRegression을 이용한 %s의 회귀분석 결과 :'%item)
-    print('훈련세트점수 : {:.2f}'.format(ridge.score(X_train, y_train)))
-    print('검증세트점수 : {:.2f}'.format(ridge.score(X_test, y_test)))
+    print('Analysis result of %s using RidgeRegression :'%item)
+    print('Training Set Score : {:.2f}'.format(ridge.score(X_train, y_train)))
+    print('Verificaiton Set Score : {:.2f}'.format(ridge.score(X_test, y_test)))
 
 
 def lassoReg(df, item, cols_using):
@@ -93,26 +93,27 @@ def lassoReg(df, item, cols_using):
 
     lasso = Lasso(alpha=0.1, max_iter=1000).fit(X=X_train, y=y_train)
   
-    print('LassoRegression을 이용한 %s의 회귀분석 결과 :'%item)
-    print('훈련세트점수 : {:.2f}'.format(lasso.score(X_train, y_train)) )
-    print('검증세트점수 : {:.2f}'.format(lasso.score(X_test, y_test)) )
+    print('Analysis result of %s using LassoRegression :'%item)
+    print('Training Set Score : {:.2f}'.format(lasso.score(X_train, y_train)) )
+    print('Verificaiton Set Score : {:.2f}'.format(lasso.score(X_test, y_test)) )
 
-    #사용한 특성수
-    print('사용한 특성수 : {}'.format(np.sum(lasso.coef_ != 0)) )
+    #Number of used properties
+    print('Number of used properties : {}'.format(np.sum(lasso.coef_ != 0)) )
 
 #########################################################################
-# 자료를 병합해주는 함수, 어떤 item인지 어느 컬럼을 기준으로 할지 받아서 병합
+# Function to merge datagrames, 'item' specifies which Retail products will be used and 
+# 'on_what' decides on a which column should dataframes merged
 def mergeForAnalysis(df1, df2, df3, item, on_what='date'):
     merged_df = pd.merge(df1.loc[df1.category==item], df2, on=on_what, how='left')
     merged_df = pd.merge(merged_df, df3, on=on_what, how='left')
     return merged_df
 
 #########################################################################
-# ols모델용 formula 생성
+# creating formula for Ordinary Least Square(ols)
 def formulaGen(target, ind_features):
     '''
-    formulaGen(목표컬럼명,[변수컬럼명1, 변수컬럼명2,...])
-    str으로 반환
+    formulaGen(target_column_name,[independent_feature_column1, independent_feature_column2,...])
+    will return str
     '''
     custom_formula = target + " ~ "
     for f in range(len(ind_features)):
